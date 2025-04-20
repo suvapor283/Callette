a5c42bf7edb60ee24e30300f156f1fb5839b7649
suvapor283 <suvapor8632@gmail.com>
2025-04-20 14:05:18 +0900

    Callette Project Start

---
+++ src/main/java/com/example/Callette/CalletteApplication.java
@@ -0,0 +1,12 @@
+package com.example.Callette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalletteApplication.class, args);
+    }
+}
