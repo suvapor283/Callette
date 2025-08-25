179128e6287c4686c692785c0e3fdb40bde8cb12
suvapor283 <suvapor8632@gmail.com>
2025-08-25 12:35:23 +0900

    Chore: configure DB connection

--- src/main/java/com/example/Callette/CalletteApplication.java
+++ src/main/java/com/example/Callette/CalletteApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalletteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalletteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CalletteApplication.class, args);
+    }
 }
