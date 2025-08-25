8c5b51fc7c5ae12c4cb8637c9245bf528d25d9e0
suvapor283 <suvapor8632@gmail.com>
2025-08-25 14:07:09 +0900

    Chore: create main page layout skeleton

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,102 +1,100 @@
-import Image from "next/image";
+import Link from "next/link";
+import {
+  Bars3Icon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/24/outline";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex min-h-screen flex-col">
+      {/* Header */}
+      <nav className="flex items-center justify-between bg-white p-4 shadow-md">
+        <div className="flex items-center space-x-4">
+          <button>
+            <Bars3Icon className="size-6" />
+          </button>
+          <h1 className="text-xl font-semibold">Calendar</h1>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex items-center space-x-2">
+          <Link href="/auth/login">
+            <Button variant="outline" className="w-[80px]">
+              sign in
+            </Button>
+          </Link>
+          <Link href="/auth/signup">
+            <Button className="w-[80px]">sign up</Button>
+          </Link>
+        </div>
+      </nav>
+
+      {/* Main */}
+      <main className="mx-auto my-6 flex min-h-screen w-[80%] min-w-[600px] flex-col p-6 border border-gray-300 rounded-lg">
+        <div className="relative flex items-center justify-between">
+          <div className="flex rounded-full bg-gray-200 p-1">
+            <a href="#">
+              <button className="rounded-full bg-white px-4 py-1 text-black shadow-md">
+                Calendar
+              </button>
+            </a>
+            <a href="#">
+              <button
+                id="diary-btn"
+                className="rounded-full px-4 py-1 text-gray-600"
+              >
+                Diary
+              </button>
+            </a>
+          </div>
+
+          <div className="absolute left-1/2 flex -translate-x-1/2 transform items-center space-x-4">
+            <button>
+              <ChevronLeftIcon className="size-4" style={{ strokeWidth: 3 }} />
+            </button>
+            <h2 className="text-center text-xl font-semibold">March 2025</h2>
+            <button>
+              <ChevronRightIcon className="size-4" style={{ strokeWidth: 3 }} />
+            </button>
+          </div>
+        </div>
+
+        <div className="mt-6 grid grid-cols-7 gap-2 text-center font-semibold text-gray-600">
+          <div>Sun</div>
+          <div>Mon</div>
+          <div>Tue</div>
+          <div>Wed</div>
+          <div>Thu</div>
+          <div>Fri</div>
+          <div>Sat</div>
+        </div>
+
+        <div className="mt-4 grid grid-cols-7 gap-2 text-sm">
+          <div className="h-24 border p-2">1</div>
+          <div className="h-24 border p-2">2</div>
+          <div className="h-24 border p-2">3</div>
+          <div className="h-24 border p-2">4</div>
+          <div className="h-24 border p-2">5</div>
+          <div className="h-24 border p-2">6</div>
+          <div className="h-24 border p-2">7</div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      {/* Footer */}
+      <footer className="flex items-center justify-between bg-white p-3 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
+        <span className="text-gray-600">© 2025 Calendar App</span>
+        <div>
+          <Link href="#">
+            <Button variant="link">Privacy</Button>
+          </Link>
+          <Link href="#">
+            <Button variant="link">Terms</Button>
+          </Link>
+          <Link href="#">
+            <Button variant="link">Help</Button>
+          </Link>
+        </div>
       </footer>
     </div>
   );
