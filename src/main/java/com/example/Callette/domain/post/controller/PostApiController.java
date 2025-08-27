c8461f21f6c2dbe3a3624e566da51199f32e8536
suvapor283 <suvapor8632@gmail.com>
2025-08-27 15:28:04 +0900

    Feat: implement Post getAll and getById endpoints

--- src/main/java/com/example/Callette/domain/post/controller/PostApiController.java
+++ src/main/java/com/example/Callette/domain/post/controller/PostApiController.java
@@ -1,11 +1,31 @@
 package com.example.Callette.domain.post.controller;
 
+import com.example.Callette.domain.post.entity.Post;
+import com.example.Callette.domain.post.service.PostService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostApiController {
+
+    private final PostService postService;
+
+    @GetMapping("")
+    public List<Post> getPosts() {
+        return this.postService.getPosts();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPost(@PathVariable("id") Long id) {
+        return this.postService.getPost(id);
+    }
 }
--- src/main/java/com/example/Callette/domain/post/service/PostService.java
+++ src/main/java/com/example/Callette/domain/post/service/PostService.java
@@ -1,9 +1,27 @@
 package com.example.Callette.domain.post.service;
 
+import com.example.Callette.domain.post.entity.Post;
+import com.example.Callette.domain.post.repository.PostRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<Post> getPosts() {
+        return this.postRepository.findAll();
+    }
+
+    public Post getPost(Long id) {
+        Optional<Post> postOptional = this.postRepository.findById(id);
+
+        return postOptional.orElseThrow(() -> new EntityNotFoundException("Post not fount"));
+    }
 }
